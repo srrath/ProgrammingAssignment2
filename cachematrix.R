d1aa581b9e4cd180bc9b23d6bdc5c72c09981244
srrath <rath.saroj@gmail.com>
2015-01-22 22:43:58 -0500

    version:1 22nd Jan

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inputMtrx = matrix()) {
+ 
+  invMtrx <- NULL
+  
+  setMtrx <- function(rstMtrx){
+      
+      inputMtrx <<- rstMtrx
+      invMtrx <<- NULL
+      
+  }
+  
+  getMtrx <- function ()inputMtrx
+  setInvMtrx <- function (inputInvMtrx) invMtrx <<- inputInvMtrx
+  getInvMtrx <- function () invMtrx
+  list(setMtrx=setMtrx, getMtrx=getMtrx,setInvMtrx=setInvMtrx,getInvMtrx=getInvMtrx   )
+    
 }
-
-
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheMtrx, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    
+    invMatrix <- cacheMtrx$getInvMtrx()
+    
+    if (!is.null(invMatrix)){
+        message("Getting Cached value")
+        return(invMatrix)
+        
+    }
+    invMatrix <- solve(cacheMtrx$getMtrx())
+    cacheMtrx$setInvMtrx(invMatrix)
+    invMatrix
+    
+
 }
+
+# Test the cache functions
+m_input <- cbind (1:2, 3:4)
+m_input
+cache_m_input <-makeCacheMatrix(m_input)
+m_input_inverse <- cacheSolve(cache_m_input)
+m_input_inverse
+# second call will retrive from Cache
+m_input_inverse <- cacheSolve(cache_m_input)
+m_input_inverse
+
+# End of Test cache functions
+
